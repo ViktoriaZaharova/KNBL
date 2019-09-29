de9eecb49e10921e4ecd82966d8be61e1424129d
ViktoriaZaharova <zaharova181114@gmail.com>
2019-09-29 14:57:57 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -23,7 +23,8 @@ $(document).ready(function() {
                 arrows: true,
                 asNavFor: '.profile-tabs__content',
                 prevArrow: '<button type="button" class="slick-prev"></button>',
-                nextArrow: '<button type="button" class="slick-next"></button>'
+                nextArrow: '<button type="button" class="slick-next"></button>',
+                adaptiveHeight: true
             });
 
             $('.profile-tabs__content:not(.slick-initialized)').slick({
@@ -32,11 +33,42 @@ $(document).ready(function() {
                 speed: 100,
                 slidesToShow: 1,
                 arrows: false,
-                asNavFor: '.profile-tabs'
+                asNavFor: '.profile-tabs',
+                adaptiveHeight: true
+            });
+
+            $('.league-wrapper__slider:not(.slick-initialized)').slick({
+                dots: false,
+                infinite: true,
+                speed: 100,
+                slidesToShow: 2,
+                centerMode: true,
+                arrows: true,
+                prevArrow: '<button type="button" class="slick-prev"></button>',
+                nextArrow: '<button type="button" class="slick-next"></button>',
+                responsive: [
+                    {
+                        breakpoint: 710,
+                        settings: {
+                            centerMode: true,
+                            centerPadding: '20px',
+                            slidesToShow: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            centerMode: true,
+                            centerPadding: '0',
+                            slidesToShow: 1
+                        }
+                    }
+                ]
             });
         } else {
             $(".profile-tabs.slick-initialized").slick("unslick");
             $(".profile-tabs__content.slick-initialized").slick("unslick");
+            $(".league-wrapper__slider.slick-initialized").slick("unslick");
         }
     });
 // slick active
